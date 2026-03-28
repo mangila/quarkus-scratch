a208d0718aa2a8c4a06806befc039b563f168a22
Erik Olsson <olsson.erik1993@gmail.com>
2026-03-28 18:34:20 +0100

    annotate CustomerEntity with @Audited and enable HTTP response compression

--- customer/api/src/main/java/com/github/mangila/customer/data/CustomerEntity.java
+++ customer/api/src/main/java/com/github/mangila/customer/data/CustomerEntity.java
@@ -7,11 +7,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import org.hibernate.annotations.Type;
+import org.hibernate.envers.Audited;
 
 import java.util.UUID;
 
 @Entity
 @Table(name = "customer")
+@Audited
 public class CustomerEntity {
 
     @Id
