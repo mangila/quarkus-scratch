2e80ae4f4ff0cb06b8649ed49aac01c2bd1cfecc
Erik Olsson <olsson.erik1993@gmail.com>
2026-03-28 13:57:01 +0100

    add JSON service for centralized ObjectMapper management

--- customer/api/src/main/java/com/github/mangila/customer/shared/JsonService.java
+++ customer/api/src/main/java/com/github/mangila/customer/shared/JsonService.java
@@ -4,6 +4,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import jakarta.enterprise.context.ApplicationScoped;
 
+/**
+ * One place for JSON inside the application scope.
+ * Instead of having injected ObjectMappers everywhere.
+ */
 @ApplicationScoped
 public class JsonService {
 
