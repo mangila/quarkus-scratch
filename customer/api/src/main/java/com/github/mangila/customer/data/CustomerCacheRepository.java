cbf8bb27b45f51b109a4fe5ead33d993416b4f5a
Erik Olsson <olsson.erik1993@gmail.com>
2026-03-31 21:21:54 +0200

    c
    Signed-off-by: Erik Olsson <olsson.erik1993@gmail.com>

--- customer/api/src/main/java/com/github/mangila/customer/data/CustomerCacheRepository.java
+++ customer/api/src/main/java/com/github/mangila/customer/data/CustomerCacheRepository.java
@@ -1,6 +1,5 @@
 package com.github.mangila.customer.data;
 
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.github.mangila.customer.web.CustomerDto;
 import io.quarkus.logging.Log;
 import jakarta.enterprise.context.ApplicationScoped;
