74b1152608169fe113e371ab5c03071383677807
Erik Olsson <olsson.erik1993@gmail.com>
2026-05-03 16:50:16 +0200

    feat: scaffold Quarkus project with GreetingResource, basic REST endpoint, integration tests, and essential configurations; add build setup, dependencies, and project structure
    Signed-off-by: Erik Olsson <olsson.erik1993@gmail.com>

---
+++ crud1/api/src/main/java/com/github/mangila/crud1/GreetingResource.java
@@ -0,0 +1,17 @@
+package com.github.mangila.crud1;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.hibernate.annotations.Type;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from Quarkus REST";
+    }
+}
---
+++ crud1/api/src/test/java/com/github/mangila/crud1/GreetingResourceIT.java
@@ -0,0 +1,9 @@
+package com.github.mangila.crud1;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+import org.junit.jupiter.api.Disabled;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
