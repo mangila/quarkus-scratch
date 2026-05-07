04952082e5879ba107fbb73913d846686be45e8c
Erik Olsson <olsson.erik1993@gmail.com>
2026-05-07 23:57:56 +0200

    feat(person): add secondary sort criteria to pagination
    - Enhanced `findPage` method in `PersonDataService` to sort by `name` and `email` for consistent and predictable pagination results.
    
    Signed-off-by: Erik Olsson <olsson.erik1993@gmail.com>

--- crud1/api/src/main/java/com/github/mangila/crud1/person/data/PersonDataService.java
+++ crud1/api/src/main/java/com/github/mangila/crud1/person/data/PersonDataService.java
@@ -20,7 +20,7 @@ public class PersonDataService {
 
   @Transactional
   public List<PersonEntity> findPage(Page page) {
-    final Sort sort = Sort.by("name");
+    final Sort sort = Sort.by("name").and("email");
     return personPostgresRepository.findAll(sort).page(page).list();
   }
 
