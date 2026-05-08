4d163cdd0d11d1cbe0fe9e8e3df62b0c09b6dad5
Erik Olsson <olsson.erik1993@gmail.com>
2026-05-08 19:44:42 +0200

    feat(client): initialize Astro project with Vue integration
    - Set up a new Astro project in the `web1/client` directory.
    - Added Vue and Partytown integrations via Astro configuration (`astro.config.mjs`).
    - Created base project files including `.gitignore`, `index.astro`, `Layout.astro`, and assets (`astro.svg`, `background.svg`, and `favicon.svg`).
    - Configured package dependencies with `package.json` and generated `package-lock.json`.
    - Defined scripts for `dev`, `build`, and `preview` modes in `package.json`.
    
    Signed-off-by: Erik Olsson <olsson.erik1993@gmail.com>

---
+++ web1/client/astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import vue from '@astrojs/vue';
+import partytown from '@astrojs/partytown';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [vue(), partytown()]
+});
